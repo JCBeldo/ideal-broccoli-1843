9d6985dbde5868a871b0ed720be9928560b3e090
Julian Beldotti <julian.beldotti@gmail.com>
2023-04-25 15:16:49 -0500

    Last one 2:15MST

--- app/controllers/plant_plots_controller.rb
+++ app/controllers/plant_plots_controller.rb
@@ -1,9 +1,10 @@
 class PlantPlotsController < ApplicationController
   def destroy
-    plant = Plant.find(params[:id])
-    plot = Plot.find(params[:id])
-    plant_plot = PlantPlot.find_by(plot_id: plot.id, plant_id: plant.id)
-    plant_plot.destroy
+    # plant = Plant.find(params[:id])
+    # plot = Plot.find(params[:plot_id])
+    # plant_plot = PlantPlot.find_by(plot_id: plot.id, plant_id: plant.id)
+    # plant_plot.destroy()
+    PlantPlot.destroy(params[:id])
   
     redirect_to plots_path
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :gardens, only: :show
   
-  resources :plots, only: :index
-
+  resources :plots, only: :index do
+  end
   resources :plant_plots, only: :destroy
 end
